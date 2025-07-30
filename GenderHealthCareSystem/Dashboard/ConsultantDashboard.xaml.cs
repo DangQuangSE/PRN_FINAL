048dc540f64ea8bff15c9da55c990fb5543ef736
dux20 <vuuong02@gmail.com>
2025-07-30 21:34:10 +0700

    dashboard

---
+++ GenderHealthCareSystem/Dashboard/ConsultantDashboard.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DAL.Entities;
+
+namespace GenderHealthCareSystem.Dashboard
+{
+    /// <summary>
+    /// Interaction logic for ConsultantDashboard.xaml
+    /// </summary>
+    public partial class ConsultantDashboard : Window
+    {
+        public User user { get; set; }
+        public ConsultantDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
